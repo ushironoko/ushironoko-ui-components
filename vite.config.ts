bf36667a62746b09d664ee71e527625f4e643bb6
ushironoko <apple19940820@gmail.com>
2021-06-06 16:53:47 +0900

    configをまとめる

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+const options = {
+  plugins: [vue()],
+  build:
+    process.env.NODE_ENV === 'development'
+      ? {}
+      : {
+          lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'my-ui-components'
+          },
+          rollupOptions: {
+            external: ['vue'],
+            output: {
+              globals: {
+                vue: 'Vue'
+              }
+            }
+          }
+        }
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default defineConfig(options);
