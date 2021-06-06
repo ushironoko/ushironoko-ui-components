2b77b23361280e27f6b9587487971f3f36532408
ushironoko <apple19940820@gmail.com>
2021-06-06 18:16:24 +0900

    add component types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 import './assets/tailwind.css';
+import PrimaryButton from './components/buttons/PrimaryButton.vue'
+import SecondaryButton from './components/buttons/SecondaryButton.vue'
+import MyDialog from './components/dialog/MyDialog.vue'
+import InputText from './components/forms/InputText.vue'
+import InputRadio from './components/forms/InputRadio.vue'
+import ContentsLoader from './components/loader/ContentsLoader.vue'
+
+export type PrimaryButton = typeof PrimaryButton
+export type SecondaryButton = typeof SecondaryButton
+export type MyDialog = typeof MyDialog
+export type InputText = typeof InputText
+export type InputRadio = typeof InputRadio
+export type ContentsLoader = typeof ContentsLoader
+
 export { default as PrimaryButton } from './components/buttons/PrimaryButton.vue'
 export { default as SecondaryButton } from './components/buttons/SecondaryButton.vue'
 export { default as MyDialog } from './components/dialog/MyDialog.vue'
