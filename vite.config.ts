05d53aec1687c5734bae7f10e97bb9493815e315
ushironoko <apple19940820@gmail.com>
2021-06-09 01:32:40 +0900

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-const options = {
+export default defineConfig({
   plugins: [vue()],
   build:
     process.env.NODE_ENV === 'development'
@@ -21,7 +21,4 @@ const options = {
             }
           }
         }
-};
-
-// https://vitejs.dev/config/
-export default defineConfig(options);
+});
